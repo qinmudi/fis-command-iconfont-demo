7b8796992c918b9ce7e757713e4e8a15b736da5f
erikqin(秦睦迪) <erikqin@tencent.com>
2016-12-09 17:54:20 +0800

    12

--- fis-conf.js
+++ fis-conf.js
@@ -1,5 +1,5 @@
 
-fis.config.set("iconfont", {
+ fis.config.set("iconfont", {
     'src': '/src/resource/svgs', //图标目录
     'dest': '/dist/resource/fonts', //产出字体目录
     'fontname': 'wiifont', //产出字体名称
